4558f7d21e91057de489fc2db095e395ecfa1a4e
marvin <bugemarvin@outlook.com>
2022-04-23 10:00:52 +0000

    c programming struture.

---
+++ 01-fundamentals/01-stucture.c
@@ -0,0 +1,26 @@
+/**
+  * File: main.c
+  * Author: Marvin kurland.
+  *
+  * Created on Saturday.
+  */
+
+#include <stdio.h>
+
+/**
+  * Description: main -as the main entry point.
+  * Return 0 or 1 Always succes.
+  */
+
+int main(void)
+{
+
+/*
+* main id the starting point of function in c.
+* () postfixed for variable intitialization.
+* printf is a identifier not a function for input output library.
+*/
+
+	printf("Hello World\n");
+	return(EXIT_SUCCES);
+}
